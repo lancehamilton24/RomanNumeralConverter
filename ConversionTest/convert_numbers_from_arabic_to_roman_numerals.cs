ec8f13d6cd8e0b7abd1a82db4b87de2c0f00d780
Lance Hamilton <lance.hamilton24@gmail.com>
2019-04-03 18:05:42 -0500

    setup test file

---
+++ ConversionTest/convert_numbers_from_arabic_to_roman_numerals.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace RomanNumeralConverter.ConversionTest
+{
+    public class convert_numbers_from_arabic_to_roman_numerals
+    {
+        [Fact]
+        public void converting_1_to_roman_should_result_I()
+        {
+            //Arrange -- Context              -- Given
+
+            //Act     -- Do the thing         -- When
+
+            //Assert  -- checking the result  -- Then
+
+        }
+    }
+}
