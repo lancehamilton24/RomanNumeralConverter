e4c6e4d7b3b687fda7c3c6effa1de2a861f4d641
Lance Hamilton <lance.hamilton24@gmail.com>
2019-04-05 22:02:50 -0500

    tested 5 and 9

--- ConversionTest/convert_numbers_from_arabic_to_roman_numerals.cs
+++ ConversionTest/convert_numbers_from_arabic_to_roman_numerals.cs
@@ -10,8 +10,30 @@ namespace RomanNumeralConverter.ConversionTest
         {
             //Arrange -- Context              -- Given
             var converter = new ConversionEngine();
-            var input = 3;
-            var expectedOutput = "III";
+            var input = 1;
+            var expectedOutput = "I";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_5_to_roman_should_result_V()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 5;
+            var expectedOutput = "V";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_9_to_roman_should_result_IX()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 9;
+            var expectedOutput = "IX";
             //Act     -- Do the thing         -- When
             var actualResult = converter.ConvertNumbers(input);
             //Assert  -- checking the result  -- Then
--- RomanNumeralConverter/ConversionEngine.cs
+++ RomanNumeralConverter/ConversionEngine.cs
@@ -11,6 +11,8 @@ namespace RomanNumeralConverter
         {
             if ((numberToConvert < 0) || (numberToConvert > 3999)) throw new ArgumentOutOfRangeException("You've entered an invalid number");
             if (numberToConvert < 1) return string.Empty;
+            if (numberToConvert >= 9) return "IX" + ConvertNumbers(numberToConvert - 9);
+            if (numberToConvert >= 5) return "V" + ConvertNumbers(numberToConvert - 5);
             if (numberToConvert >= 1) return "I" + ConvertNumbers(numberToConvert - 1);
             throw new ArgumentOutOfRangeException("Error");
         }
