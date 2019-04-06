9005205ea73e89a6f84f2968e54026b1aae07bbe
Lance Hamilton <lance.hamilton24@gmail.com>
2019-04-05 22:26:29 -0500

    completed all tests

--- ConversionTest/convert_numbers_from_arabic_to_roman_numerals.cs
+++ ConversionTest/convert_numbers_from_arabic_to_roman_numerals.cs
@@ -39,5 +39,225 @@ namespace RomanNumeralConverter.ConversionTest
             //Assert  -- checking the result  -- Then
             Assert.Equal(expectedOutput, actualResult);
         }
+        public void converting_12_to_roman_should_result_XII()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+        var input = 12;
+        var expectedOutput = "XII";
+        //Act     -- Do the thing         -- When
+        var actualResult = converter.ConvertNumbers(input);
+        //Assert  -- checking the result  -- Then
+        Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_16_to_roman_should_result_XVI()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+        var input = 16;
+        var expectedOutput = "XVI";
+        //Act     -- Do the thing         -- When
+        var actualResult = converter.ConvertNumbers(input);
+        //Assert  -- checking the result  -- Then
+        Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_29_to_roman_should_result_XXIX()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 29;
+            var expectedOutput = "XXIX";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_44_to_roman_should_result_XLIV()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 44;
+            var expectedOutput = "XLIV";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_45_to_roman_should_result_XLV()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 45;
+            var expectedOutput = "XLV";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_68_to_roman_should_result_LXVIII()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 68;
+            var expectedOutput = "LXVIII";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_83_to_roman_should_result_LXXXIII()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 83;
+            var expectedOutput = "LXXXIII";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_97_to_roman_should_result_XCVII()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 97;
+            var expectedOutput = "XCVII";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_99_to_roman_should_result_XCIX()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 99;
+            var expectedOutput = "XCIX";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_500_to_roman_should_result_D()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 500;
+            var expectedOutput = "D";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_501_to_roman_should_result_DI()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 500;
+            var expectedOutput = "DI";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_649_to_roman_should_result_DCXLIX()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 649;
+            var expectedOutput = "DCXLIX";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_798_to_roman_should_result_DCCXCVIII()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 798;
+            var expectedOutput = "DCCXCVIII";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_891_to_roman_should_result_DCCCXCI()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 891;
+            var expectedOutput = "DCCCXCI";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_1000_to_roman_should_result_M()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 1000;
+            var expectedOutput = "M";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_1004_to_roman_should_result_MIV()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 1004;
+            var expectedOutput = "MIV";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_1006_to_roman_should_result_MVI()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 1006;
+            var expectedOutput = "MVI";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_1023_to_roman_should_result_MXXIII()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 1023;
+            var expectedOutput = "MXXIII";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_2014_to_roman_should_result_MMXIV()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 2014;
+            var expectedOutput = "MMXIV";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
+        public void converting_3999_to_roman_should_result_MMMCMX()
+        {
+            //Arrange -- Context              -- Given
+            var converter = new ConversionEngine();
+            var input = 3999;
+            var expectedOutput = "MMMCMX";
+            //Act     -- Do the thing         -- When
+            var actualResult = converter.ConvertNumbers(input);
+            //Assert  -- checking the result  -- Then
+            Assert.Equal(expectedOutput, actualResult);
+        }
     }
 }
--- RomanNumeralConverter/ConversionEngine.cs
+++ RomanNumeralConverter/ConversionEngine.cs
@@ -11,6 +11,26 @@ namespace RomanNumeralConverter
         {
             if ((numberToConvert < 0) || (numberToConvert > 3999)) throw new ArgumentOutOfRangeException("You've entered an invalid number");
             if (numberToConvert < 1) return string.Empty;
+            if (numberToConvert >= 3999) return "MMMCMX" + ConvertNumbers(numberToConvert - 3999);
+            if (numberToConvert >= 2014) return "MMXIV" + ConvertNumbers(numberToConvert - 2014);
+            if (numberToConvert >= 1023) return "MXXIII" + ConvertNumbers(numberToConvert - 1023);
+            if (numberToConvert >= 1006) return "MVI" + ConvertNumbers(numberToConvert - 1006);
+            if (numberToConvert >= 1004) return "MIV" + ConvertNumbers(numberToConvert - 1004);
+            if (numberToConvert >= 1000) return "M" + ConvertNumbers(numberToConvert - 1000);
+            if (numberToConvert >= 891) return "DCCCXCI" + ConvertNumbers(numberToConvert - 891);
+            if (numberToConvert >= 798) return "DCCXCVII" + ConvertNumbers(numberToConvert - 798);
+            if (numberToConvert >= 649) return "DCXLIX" + ConvertNumbers(numberToConvert - 649);
+            if (numberToConvert >= 501) return "DI" + ConvertNumbers(numberToConvert - 501);
+            if (numberToConvert >= 500) return "D" + ConvertNumbers(numberToConvert - 500);
+            if (numberToConvert >= 99) return "XCIX" + ConvertNumbers(numberToConvert - 99);
+            if (numberToConvert >= 97) return "XCVII" + ConvertNumbers(numberToConvert - 97);
+            if (numberToConvert >= 83) return "LXXXIII" + ConvertNumbers(numberToConvert - 83);
+            if (numberToConvert >= 68) return "LXVIII" + ConvertNumbers(numberToConvert - 68);
+            if (numberToConvert >= 45) return "XLV" + ConvertNumbers(numberToConvert - 45);
+            if (numberToConvert >= 44) return "XLIV" + ConvertNumbers(numberToConvert - 44);
+            if (numberToConvert >= 29) return "XXIX" + ConvertNumbers(numberToConvert - 29);
+            if (numberToConvert >= 16) return "XVI" + ConvertNumbers(numberToConvert - 16);
+            if (numberToConvert >= 12) return "XII" + ConvertNumbers(numberToConvert - 12);
             if (numberToConvert >= 9) return "IX" + ConvertNumbers(numberToConvert - 9);
             if (numberToConvert >= 5) return "V" + ConvertNumbers(numberToConvert - 5);
             if (numberToConvert >= 1) return "I" + ConvertNumbers(numberToConvert - 1);
