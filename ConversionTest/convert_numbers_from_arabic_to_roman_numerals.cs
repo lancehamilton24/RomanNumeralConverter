4f75a29af70084c106054a139bfcee0469667b7b
Lance Hamilton <lance.hamilton24@gmail.com>
2019-04-05 21:54:44 -0500

    tested numbers 1 - 4

--- ConversionTest/convert_numbers_from_arabic_to_roman_numerals.cs
+++ ConversionTest/convert_numbers_from_arabic_to_roman_numerals.cs
@@ -9,11 +9,13 @@ namespace RomanNumeralConverter.ConversionTest
         public void converting_1_to_roman_should_result_I()
         {
             //Arrange -- Context              -- Given
-
+            var converter = new ConversionEngine();
+            var input = 2;
+            var expectedOutput = "II";
             //Act     -- Do the thing         -- When
-
+            var actualResult = converter.ConvertNumbers(input);
             //Assert  -- checking the result  -- Then
-
+            Assert.Equal(expectedOutput, actualResult);
         }
     }
 }
